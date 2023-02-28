ce4ec4ce4f999d71b9fb5af60c4c6c749ed221ea
haohaodiniyao <1270305593@qq.com>
2023-02-28 19:53:05 +0800

    添加common-langs

--- src/main/java/com/example/demo202303/Demo202303Application.java
+++ src/main/java/com/example/demo202303/Demo202303Application.java
@@ -1,5 +1,6 @@
 package com.example.demo202303;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo202303Application {
 
     public static void main(String[] args) {
+//        if(StringUtils.contains())
         SpringApplication.run(Demo202303Application.class, args);
     }
 
