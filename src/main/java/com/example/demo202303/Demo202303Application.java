d26a4fb40f375f9cc27386ae1ee208caa4eb842e
haohaodiniyao <1270305593@qq.com>
2023-02-28 19:34:07 +0800

    Initial commit

---
+++ src/main/java/com/example/demo202303/Demo202303Application.java
@@ -0,0 +1,13 @@
+package com.example.demo202303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo202303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo202303Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo202303/Demo202303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo202303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo202303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
