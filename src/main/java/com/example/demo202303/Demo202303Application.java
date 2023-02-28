08d9ad4621ed18aaaa663ef52b6b623f6c9283b4
haohaodiniyao <1270305593@qq.com>
2023-02-28 19:42:20 +0800

    Revert "我的注释"
    This reverts commit f89b0eff

--- src/main/java/com/example/demo202303/Demo202303Application.java
+++ src/main/java/com/example/demo202303/Demo202303Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo202303Application {
 
     public static void main(String[] args) {
-        //我的测试
         SpringApplication.run(Demo202303Application.class, args);
     }
 
--- src/main/java/com/example/demo202303/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo202303;
-
-/**
- * 我的类
- */
-public class Test {
-}
